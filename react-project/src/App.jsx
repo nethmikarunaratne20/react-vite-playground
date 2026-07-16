e9a5b20d26f9993230b66fa41e7d7aaf954c9384
nethmikarunaratne20 <nethmi.20241218@iit.ac.lk>
2026-07-16 23:24:55 +0530

    Displayed images with react and used fragments

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css';
+import "./App.css";
+import img1 from "./images/img1.jpg";
 
-function Header({name,year}){
+function Header({ name, year }) {
   return (
     <header>
       <h1>{name} Kitchen</h1>
       <p>Copyright {year}</p>
     </header>
-  )
-};
-
+  );
+}
 
 const items = [
   "Pizza Margherita",
@@ -16,7 +16,7 @@ const items = [
   "Lasagna Al Forno",
   "Risotto ai Funghi",
   "Tiramisu",
-  "Gelato"
+  "Gelato",
 ];
 
 const dishObjects = items.map((dish, i) => ({
@@ -24,23 +24,34 @@ const dishObjects = items.map((dish, i) => ({
   title: dish,
 }));
 
-
-function Main({dishes}){
+function Main({ dishes }) {
   return (
-    <ul>
-      {dishes.map((dish) => 
-        <li key={dish.id} style = {{listStyleType: "none"}}> {dish.title} </li>)}
-    </ul>
-  )
-};
+    <>
+    <div>
+      <h2>Welcome to our restaurant! We offer a variety of delicious Italian dishes.</h2>
+    </div>
+    <main>
+      <img src={img1} height={200} alt="An image of the restaurant"/>
+      <ul>
+        {dishes.map((dish) => (
+          <li key={dish.id} style={{ listStyleType: "none" }}>
+            {" "}
+            {dish.title}{" "}
+          </li>
+        ))}
+      </ul>
+    </main>
+    </>
+  );
+}
 
 function App() {
-  return ( 
+  return (
     <div>
       <Header name="Italian" year={new Date().getFullYear()} />
-      <Main dishes = {dishObjects} />
+      <Main dishes={dishObjects} />
     </div>
-  )
-};
+  );
+}
 
 export default App;
