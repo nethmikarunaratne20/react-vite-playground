9efb2f849adbd5ba88a2eb81cd852ef1ea66c387
nethmikarunaratne20 <nethmi.20241218@iit.ac.lk>
2026-07-21 19:32:24 +0530

    Created a project with Next.js

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,10 +1,9 @@
-import {useState} from "react";
+import { useEffect, useReducer } from "react";
 import "./App.css";
 import img1 from "./images/img1.jpg";
 
-
-// Here the Prop is destructured in the function parameter list, 
-// so we can directly access the 'name' and 'year' props without 
+// Here the Prop is destructured in the function parameter list,
+// so we can directly access the 'name' and 'year' props without
 // needing to reference a 'props' object.
 function Header({ name, year }) {
   return (
@@ -26,35 +25,38 @@ const items = [
   "Gelato",
 ];
 
-// Here we are using the map function to create a new array of objects, 
-// where each object has an 'id' and a 'title' property. 
-// The 'id' is the index of the dish in the original array, 
+// Here we are using the map function to create a new array of objects,
+// where each object has an 'id' and a 'title' property.
+// The 'id' is the index of the dish in the original array,
 // and the 'title' is the name of the dish.
 const dishObjects = items.map((dish, i) => ({
   id: i,
   title: dish,
 }));
 
-// The Main component is responsible for rendering the main content of 
-// the application. It receives an array of dish objects as a prop and 
-// maps over them to create a list of dishes. Each dish is displayed as 
-// a list item, and the 'key' prop is set to the 'id' of the dish to help 
+// The Main component is responsible for rendering the main content of
+// the application. It receives an array of dish objects as a prop and
+// maps over them to create a list of dishes. Each dish is displayed as
+// a list item, and the 'key' prop is set to the 'id' of the dish to help
 // React identify which items have changed, are added, or are removed.
-function Main({ dishes }) {
+function Main({ dishes, openStatus, onStatus }) {
   return (
-    // Here '<>' is a React Fragment, which allows you to group multiple 
+    // Here '<>' is a React Fragment, which allows you to group multiple
     // elements without adding extra nodes to the DOM.
     // this can also be written as <React.Fragment>...</React.Fragment>
-    // this method requires importing React at the top of the file, 
+    // this method requires importing React at the top of the file,
     // which is not necessary in React 17 and later.
     <>
       <div>
+        {/* The button  below has used nested components to handle the state change */}
+        <button onClick={() => onStatus(true)}>I want to be open</button>
         <h2>
           Welcome to our restaurant! We offer a variety of delicious Italian
-          dishes.
+          dishes! {""}
+          {openStatus ? "Open" : "Closed"}
         </h2>
       </div>
-      <main>        
+      <main>
         <img src={img1} height={200} alt="An image of the restaurant" />
         <ul>
           {dishes.map((dish) => (
@@ -73,20 +75,25 @@ function Main({ dishes }) {
 function App() {
   // const what = useState();
   // console.log(what);  ---> this returns an array with 2 elements.
-  
-  const [status, setStatus] = useState(true);
-  console.log(status);
+
+  //const [status, setStatus] = useState(true);
+  //console.log(status);
+
+  // here the useReducer is used to manage the state.
+  const [status, toggle] = useReducer((status) => !status, true);
+
+  useEffect(() => {
+    console.log(`The restaurant is ${status ? "open" : "closed"}`);
+  }, [status]);
 
   return (
     <div>
       {/* displays the current state of the restaurant */}
       <h1>The Restaurant is currently {status ? "open" : "closed"}</h1>
-      <button onClick={() => setStatus(!status)}>
-        {status ? "Close" : "Open"} Restaurant
-      </button>
-      
+      <button onClick={toggle}>{status ? "Close" : "Open"} Restaurant</button>
+
       <Header name="Italian" year={new Date().getFullYear()} />
-      <Main dishes={dishObjects} />
+      <Main dishes={dishObjects} openStatus={status} onStatus={toggle} />
     </div>
   );
 }
